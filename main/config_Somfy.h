488bf52e15a4802d8d1f678ff40ce584c10c0bf8
Andrew Sozonnyk <asozonnyk@nvidia.com>
2021-12-31 16:39:44 +1100

    Adding personal configuration

--- main/config_Somfy.h
+++ main/config_Somfy.h
@@ -31,10 +31,10 @@ extern void setupSomfy();
 extern void MQTTtoSomfy(char* topicOri, JsonObject& RFdata);
 /*----------------------------USER PARAMETERS-----------------------------*/
 #define EEPROM_ADDRESS_START 0
-#define SOMFY_REMOTE_NUM     1
+#define SOMFY_REMOTE_NUM     2
 
 // Do not change the order of the remotes, because based on the index the rolling codes are stored
-const uint32_t somfyRemotes[SOMFY_REMOTE_NUM] = {0x5184c8};
+const uint32_t somfyRemotes[SOMFY_REMOTE_NUM] = {0x5184c8, 0xba24d0};
 
 /*-------------DEFINE YOUR MQTT PARAMETERS BELOW----------------*/
 #define subjectMQTTtoSomfy "/commands/MQTTtoSomfy"
